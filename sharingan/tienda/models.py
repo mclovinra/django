d4e09826c4ba7684993f5a431e0949ea55c4504d
diego <die.penaloza@duocuc.cl>
2024-06-26 00:19:37 -0400

    model producto

--- sharingan/tienda/models.py
+++ sharingan/tienda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    id_prod = models.IntegerField(primary_key = True)
+    titulo_prod = models.CharField(max_length = 50)
+    volumen_prod = models.IntegerField()
+    desc_prod = models.CharField(max_length = 200)
+    precio_prod = models.IntegerField()
+    stock_prod = models.IntegerField()
+    editorial_prod = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.titulo_prod
+
+class Categoria(models.Model):
+    id_cat =  models.IntegerField(primary_key = True)
+    desc_cat = models.CharField(max_length = 25)
