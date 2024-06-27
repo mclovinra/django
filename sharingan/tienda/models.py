8e9b3f18377dac833ad580bdf1735f7b39385b38
diego <die.penaloza@duocuc.cl>
2024-06-27 00:07:30 -0400

    sql

--- sharingan/tienda/models.py
+++ sharingan/tienda/models.py
@@ -12,6 +12,21 @@ class Producto(models.Model):
     def __str__(self):
         return self.titulo_prod
 
+
 class Categoria(models.Model):
     id_cat =  models.IntegerField(primary_key = True)
     desc_cat = models.CharField(max_length = 25)
+
+    def __str__(self):
+        return self.desc_cat
+        
+
+class CategoriaProd(models.Model):
+    id_catego = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    id_produc = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('id_catego', 'id_produc')
+
+    def __str__(self):
+        return f'{self.id_catego.desc_cat} - {self.id_produc.titulo_prod}'
