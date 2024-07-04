17c287c6791631360e54ce125a0878f4e5faecba
diego <die.penaloza@duocuc.cl>
2024-07-04 15:42:26 -0400

    static

--- sharingan/sharingan/settings.py
+++ sharingan/sharingan/settings.py
@@ -94,6 +94,9 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
