0cedcb99fe0c797c1923c3e260f17819af67a447
diego <die.penaloza@duocuc.cl>
2024-07-04 16:02:35 -0400

    static

--- sharingan/sharingan/settings.py
+++ sharingan/sharingan/settings.py
@@ -29,13 +29,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sharingan.urls'
@@ -91,11 +91,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Ruta donde se recolectarán los archivos estáticos en Render
+    STATIC_ROOT = '/opt/render/project/src/sharingan/staticfiles'
+    # Ruta donde se encuentran los archivos estáticos en tu proyecto
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static")
+    ]
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
