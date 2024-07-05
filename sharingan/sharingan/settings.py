2780610c6c7a9fc9b3bdbe2b1860c2fc9cd91157
diego <die.penaloza@duocuc.cl>
2024-07-05 12:21:57 -0400

    .

--- sharingan/sharingan/settings.py
+++ sharingan/sharingan/settings.py
@@ -103,8 +103,8 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTHENTICATION_BACKENDS = [
     'clientes.auth_backends.ClienteBackend',
